81ed4ebd61ee450dd0ece8d80cfb07c6aa9a847c
Glitch (functional-politician) <none>
2019-09-11 21:17:41 +0000

    🐛🍼 Checkpoint ./src/components/collection/collection-avatar.js:867163/1527 ./src/components/collection/collection-avatar.styl:867163/96 ./src/components/collection/container.styl:867163/13

--- src/components/collection/collection-avatar.js
+++ src/components/collection/collection-avatar.js
@@ -3,18 +3,23 @@ import PropTypes from 'prop-types';
 import Image from 'Components/images/image';
 import { CollectionAvatar as DefaultCollectionAvatar } from 'Components/images/avatar';
 import { FALLBACK_AVATAR_URL, getProjectAvatarUrl } from 'Models/project';
-
-
+import styles from './collection-avatar.styl';
 
 const CollectionCollage = ({ collection }) => {
-  if(collection.project.length === 0){
+  if(collection.projects.length === 0){
     return(
       <DefaultCollectionAvatar collection={collection} />
     )
-  }
-  return(
-    <Image src={getProjectAvatarUrl(collection.projects[0])} alt="" />
-  )
+  }else{
+    // get the first 3 projects
+    return(
+      { collection.projects.slice(0, 3).map((item, index) => (
+        <div className={`item${index}`}>
+          <Image className={styles.avatar} src={getProjectAvatarUrl(item)} alt="" />
+        </div>
+      );
+    }
+    // <Image className={styles.avatar} src={getProjectAvatarUrl(collection.projects[0])} alt="" />
 }
 
 CollectionCollage.propTypes = {
