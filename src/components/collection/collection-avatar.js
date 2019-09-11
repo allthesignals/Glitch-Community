a9afea079bc0d07665891e99f89c17e55e5c9ec3
Glitch (functional-politician) <none>
2019-09-11 21:41:40 +0000

    👕👾 Checkpoint ./src/components/collection/collection-avatar.js:867163/527 ./src/components/collection/collection-avatar.styl:867163/103

--- src/components/collection/collection-avatar.js
+++ src/components/collection/collection-avatar.js
@@ -4,11 +4,12 @@ import Image from 'Components/images/image';
 import { CollectionAvatar as DefaultCollectionAvatar } from 'Components/images/avatar';
 import { FALLBACK_AVATAR_URL, getProjectAvatarUrl } from 'Models/project';
 import styles from './collection-avatar.styl';
+import classNames from 'classnames';
 
 const CollectionAvatar = ({ collection }) => (
   <div className={styles.avatarContainer} style={{backgroundColor: collection.coverColor}}>
     { collection.projects.slice(0, 3).map((item, index) => (
-      <div className={`item-${index}`}>
+      <div className={classNames(styles.projectAvatar, `item-${index}`)}>
         <Image className={styles.avatar} src={getProjectAvatarUrl(item)} alt="" />
       </div>
     ))}
