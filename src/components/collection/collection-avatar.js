609aad25f11703c144238c91d9f485bc66d35ead
Glitch (functional-politician) <none>
2019-09-11 20:48:32 +0000

    ✂️🎱 Checkpoint ./src/components/collection/collection-avatar.js:867163/264 ./src/components/collection/container.js:867163/648

--- src/components/collection/collection-avatar.js
+++ src/components/collection/collection-avatar.js
@@ -4,13 +4,9 @@ import Image from 'Components/images/image';
 import { FALLBACK_AVATAR_URL, getProjectAvatarUrl } from 'Models/project';
 
 
-const CollectionAvatar = ({ collection }) => {
-  render(
-    <div>
-      <Image className={styles.avatar} src={getProjectAvatarUrl(collection.projects[0])} defaultSrc={FALLBACK_AVATAR_URL} alt="" />
-    </div> 
-  )
-}
+const CollectionAvatar = ({ collection }) => (
+  <Image className={styles.avatar} src={getProjectAvatarUrl(collection.projects[0])} alt="" />
+);
 
 CollectionAvatar.propTypes = {
   collection: PropTypes.object.isRequired,
--- src/components/collection/container.js
+++ src/components/collection/container.js
@@ -17,6 +17,7 @@ import EditCollectionColor from 'Components/collection/edit-collection-color-pop
 import AuthDescription from 'Components/fields/auth-description';
 import { CollectionAvatar as DefaultCollectionAvatar, BookmarkAvatar } from 'Components/images/avatar';
 import { CollectionAvatar } from 'Components/collection/collection-avatar'
+import { FALLBACK_AVATAR_URL, getProjectAvatarUrl } from 'Models/project';
 import { CollectionLink } from 'Components/link';
 import Arrow from 'Components/arrow';
 import { useCollectionCurator } from 'State/collection';
@@ -57,12 +58,8 @@ const CollectionContainer = ({ collection, showFeaturedProject, isAuthorized, pr
   if (myStuffIsEnabled && collection.isMyStuff) {
     avatar = <BookmarkAvatar width="50%" />;
   } else if (collection.avatarUrl) {
-    {/* temporarily replace this for testing purposes
-    avatar = <Image src={collection.avatarUrl} alt="" />;
     avatar = <CollectionAvatar collection={collection} />;
-    */}
-    avatar = <Image src={collection.avatarUrl} alt="" />;
-    
+    // avatar = <Image className={styles.avatar} src={getProjectAvatarUrl(collection.projects[0])} defaultSrc={FALLBACK_AVATAR_URL} alt="" />    
   } else {
     avatar = <DefaultCollectionAvatar collection={collection} />;
   }
