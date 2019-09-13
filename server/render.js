3b244d50c12572e76decdc4554b03e3b0499a2db
Glitch (profuse-tea) <none>
2019-09-13 21:23:34 +0000

    🎮👾 Checkpoint ./server/render.js:97325/56

--- server/render.js
+++ server/render.js
@@ -55,7 +55,7 @@ const requireClient = () => {
 
 const React = require('react');
 const ReactDOMServer = require('react-dom/server');
-const { ServerStyleSheet } = require('styled-components');
+const { ServerStyleSheet } = require('node_modules/@fogcreek/shared-components/node_modules/styled-components');
 setImmediate(() => {
   try {
     requireClient();
