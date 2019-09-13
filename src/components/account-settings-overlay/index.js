e5877db32ee451612b361502506a1710b7d998ec
Glitch (community-staging) <none>
2019-09-13 14:24:09 +0000

    🐮👆 Checkpoint ./src/components/account-settings-overlay/index.js:9/255

--- src/components/account-settings-overlay/index.js
+++ src/components/account-settings-overlay/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Icon, Popover, SegmentedButton } from '@fogcreek/shared-components';
+import { Icon, SegmentedButton } from '@fogcreek/shared-components';
 
 import Text from 'Components/text/text';
 import { Overlay, OverlaySection, OverlayTitle, OverlayBackground } from 'Components/overlays';
