9867a91285f1f6f489fa40e49408aa42f382ade5
Glitch (community-staging) <none>
2019-09-16 14:16:04 +0000

    🔨🎹 Checkpoint ./src/presenters/pages/privacy-notifications.js:271885/3769

--- src/presenters/pages/privacy-notifications.js
+++ src/presenters/pages/privacy-notifications.js
@@ -5,21 +5,25 @@ import { Icon } from '@fogcreek/shared-components';
 import Layout from 'Components/layout';
 import { AnalyticsContext, useTrackedFunc } from 'State/segment-analytics';
 
+// TODO: redux state
+const 
+
+
 const PreferenceItemWrap = styled.label`
   display: flex;
   align-items: flex-start;
-`
+`;
 
 const PreferenceTitle = styled.h4`
   font-size: var(--sizes-normal);
   font-weight: normal;
   margin: 0 0 var(--space-1);
-`
+`;
 
 const PreferenceDescription = styled.p`
   font-size: var(--sizes-small);
   margin: 0;
-`
+`;
 
 const PreferenceItem = ({ icon, title, description, value, onChange }) => (
   <PreferenceItemWrap>
@@ -33,30 +37,23 @@ const PreferenceItem = ({ icon, title, description, value, onChange }) => (
 );
 
 const PrivacyNotificationsTab = () => {
-  const privacySettings = usePrivacySettings()
-  const dispatch = useDispatch() 
-  
+  const privacySettings = usePrivacySettings();
+  const dispatch = useDispatch();
+
   return (
     <section>
       <PreferencesHeader>
         <h3>Privacy</h3>
         <PreferencesSubtitle>
-          Activity Sharing
-          {" "}
-          <Switch value={privacySettings.privacyMaster} onChange={(value) => dispatch(actions.setPrivacyMaster(value))} /> 
+          Activity Sharing <Switch value={privacySettings.privacyMaster} onChange={(value) => dispatch(actions.setPrivacyMaster(value))} />
         </PreferencesSubtitle>
         <PreferencesDescription>
           Activity Sharing helps other Glitch creators discover how you're interacting with their projects.
         </PreferencesDescription>
-      </ PreferencesHeader>
-      <
-      
+      </PreferencesHeader>
     </section>
-  )
-}
-
-
-
+  );
+};
 
 // TODO: does this page already exist?
 const SettingsPageContainer = () => (
