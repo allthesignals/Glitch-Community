46583ff11ee42045f6b1e4ef6b60aca4b2730dfa
Glitch (community-staging) <none>
2019-09-16 14:02:33 +0000

    🚙💨 Checkpoint ./src/presenters/pages/privacy-notifications.js:271885/2950

---
+++ src/presenters/pages/privacy-notifications.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+import Layout from 'Components/layout';
+import { AnalyticsContext, useTrackedFunc } from 'State/segment-analytics';
+
+const PreferenceItemWrap = styled.label`
+  display: flex;
+  align-items: flex-start;
+`
+
+const PreferenceTitle = styled.h3`
+  font-size: var(--sizes-normal);
+  font-weight: normal;
+  margin: 0 0 var(--space-1);
+`
+
+const PreferenceDescription = srtt
+
+const PreferenceItem = ({ icon, title, description, value, onChange }) => (
+  <PreferenceItemWrap>
+    <Icon icon={icon} />
+    <div>
+      <PreferenceTitle>{title}</PreferenceTitle>
+      {description && <PreferenceDescription>{description}</PreferenceDescription>}
+    </div>
+    <input type="checkbox" checked={value} onChange={(e) => onChange(e.target.checked)}
+  </PreferenceItemWrap>
+)
+
+
+
+// TODO: does this page already exist?
+const SettingsPageContainer = () => (
+  <Layout>
+    <AnalyticsContext properties={{ origin: 'settings' }}>
+      <PrivacyNotificationsTab />
+    </AnalyticsContext>
+  </Layout>
+);
