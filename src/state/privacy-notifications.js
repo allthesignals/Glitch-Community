cf56601259d74064098430e85032e3baabc6df92
Glitch (community-staging) <none>
2019-09-16 17:18:49 +0000

    📍📼 Checkpoint ./src/state/privacy-notifications.js:271885/248

--- src/state/privacy-notifications.js
+++ src/state/privacy-notifications.js
@@ -1,3 +1,16 @@
 u import { createSlice } from 'redux-starter-kitimport { useSelector } from 'react-redux'
 
-export const { reducer, actions } = createSlice()
+export const { reducer, actions } = createSlice({
+  slice: 'privacyAndNotificationSettings',
+  reducers: {
+    
+  },
+})
+
+export const usePrivacyNotificationsSettings = () => ({
+  privacyMaster: true,
+  notificationsMaster: true,
+  mutedUsers: [],
+  mutedProjects: [],
+});
+
