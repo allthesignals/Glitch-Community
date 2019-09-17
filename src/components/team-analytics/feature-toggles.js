0af1445bca69333a8ddd00552bcfde2b1f0661f3
Glitch (community-staging) <none>
2019-09-17 21:28:47 +0000

    🐛🏖 Checkpoint ./src/components/team-analytics/feature-toggles.js:277163/416

---
+++ src/components/team-analytics/feature-toggles.js
@@ -0,0 +1,14 @@
+import { setLogLevel, setLogger, logging, createInstance } from '@optimizely/optimizely-sdk';
+
+setLogLevel('info');
+setLogger(logging.createLogger())
+
+const optimizelyClientInstance = createInstance({
+  sdkKey: process.env.OPTIMIZELY_KEY || 'SUV4dn9FiCp1osqWQUZr82',
+  datafileOptions: {
+    autoUpdate: true,
+    updateInterval: 1000,  // 1 second in milliseconds
+  },
+});
+
+export default optimizelyClientInstance;
