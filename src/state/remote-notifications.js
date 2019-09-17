3a656483292d03b4fbd1df57a7999c3b783e7649
Glitch (community-staging) <none>
2019-09-17 15:03:39 +0000

    🎁🌸 Checkpoint ./src/state/remote-notifications.js:271885/266

---
+++ src/state/remote-notifications.js
@@ -0,0 +1,9 @@
+import { createSlice } from 'redux-starter-kit'
+import { useSelector } from 'react-redux'
+
+export const { reducer, actions } = createSlice({
+  slice: 'remoteNotifications',
+  reducer: {
+    
+  }
+})
