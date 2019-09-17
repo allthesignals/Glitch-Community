c49b85efbed8a9c8c61f8734c9b86d91ca00bd56
Glitch (community-staging) <none>
2019-09-17 19:33:51 +0000

    📇💸 Checkpoint ./src/state/app-mounted.js:271885/668

--- src/state/app-mounted.js
+++ src/state/app-mounted.js
@@ -1,11 +1,13 @@
-import { useEffect } from 'react'
+import { useEffect } from 'react';
 import { createAction } from 'redux-starter-kit';
-import { useDispatch } from 'react-redux'
+import { useDispatch } from 'react-redux';
 
 export const appMounted = createAction('appMounted');
 
 export const useAppMounted = () => {
+  const dispatch = useDispatch();
+  const 
   useEffect(() => {
-    dispatch(appMounted())
-  }, [])
-}
+    dispatch(appMounted());
+  }, []);
+};
