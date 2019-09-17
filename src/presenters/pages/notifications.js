d01ab83bad97fb9bea5c92186f56c65e29c3985a
Glitch (community-staging) <none>
2019-09-17 14:51:36 +0000

    📉🗻 Checkpoint ./src/presenters/pages/notifications.js:271885/1422

---
+++ src/presenters/pages/notifications.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import styled from 'styled-components'
+import { useDispatch } from 'react-redux'
+import { Button, SegmentedButton, Icon, Popover, Actions } from '@fogcreek/shared-components'
+import Layout from 'Components/layout'
+import { useCurrentUser } from 'State/current-user'
+
+
+const NotificationsPage = () => {
+  
+  return (
+    <>
+      <header>
+        <h2>
+          Notifications
+          {" "}
+          <Button as="a" href="/settings#privacy-and-notifications">Edit notification settings</Button>
+        </h2>
+        <SegmentedButton
+      </header>
+    </>
+  )
+}
+
+const NotificationsPageContainer = () => {
+  const { currentUser } = useCurrentUser()
+  return <Layout>{currentUser.login ? <NotificationsPage /> : <div/> }</Layout>
+}
