40f7fc3b98663453d7346686c97b4823ad339c3e
Glitch (community-staging) <none>
2019-09-17 19:59:17 +0000

    🎑🍦 Checkpoint ./src/presenters/pages/notifications.js:271885/3

--- src/presenters/pages/notifications.js
+++ src/presenters/pages/notifications.js
@@ -132,7 +132,7 @@ const RemixNotification = ({ notification }) => {
   return (
     <NotificationBase
       href={getProjectLink(remixProject)}
-      label
+      label=""
       notification={notification}
       icon="microphone"
       options={options}
