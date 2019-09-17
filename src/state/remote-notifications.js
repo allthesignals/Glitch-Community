12594e33a37d0e368337355a12f0daee2c52db17
Glitch (community-staging) <none>
2019-09-17 15:13:35 +0000

    👑🐥 Checkpoint ./src/state/remote-notifications.js:271885/2680

--- src/state/remote-notifications.js
+++ src/state/remote-notifications.js
@@ -1,6 +1,6 @@
-import { createSlice } from 'redux-starter-kit'
-import { useSelector } from 'react-redux'
-import { sumBy } from 'lodash'
+import { createSlice } from 'redux-starter-kit';
+import { useSelector } from 'react-redux';
+import { sumBy } from 'lodash';
 
 export const { reducer, actions } = createSlice({
   slice: 'remoteNotifications',
@@ -12,19 +12,22 @@ export const { reducer, actions } = createSlice({
   reducer: {
     requestedNotifications: (state) => ({ ...state, status: 'loading' }),
     loadedNotificationsFromAPI: (state, { payload: { notifications, nextPage } }) => ({ status: 'ready', notifications, nextPage }),
+    loadedMoreNotificationsFromAPI: (state, { payload: { notifications, nextPage } }) => ({
+      status: 'ready',
+      notifications: state.notifications.concat(notifications),
+      nextPage,
+    }),
     loadedNewNotificationFromSocket: (state, { payload }) => {
-      state.notifications.unshift(payload)
+      state.notifications.unshift(payload);
     },
     updatedNotificationStatus: (state, { payload: { id, status } }) => {
-      state.notifications.find(n => n.id).status = status
+      state.notifications.find((n) => n.id).status = status;
     },
-  }
-})
+  },
+});
 
-export const handlers = {
-  
-}
+export const handlers = {};
 
-export const useNotifications = () => useSelector(state => state.remoteNotifications.notifications)
+export const useNotifications = () => useSelector((state) => state.remoteNotifications.notifications);
 
-export const useUnreadNotificationsCount = () => useSelector(state => sumBy(state.remoteNotifications.notifications, (n) => n.status === 'unread'))
+export const useUnreadNotificationsCount = () => useSelector((state) => sumBy(state.remoteNotifications.notifications, (n) => n.status === 'unread'));
