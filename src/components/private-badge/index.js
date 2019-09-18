6bf315f1fe87f768b7945c8092ab368670734da9
Glitch (community-staging) <none>
2019-09-18 19:22:46 +0000

    🌉💨 Checkpoint ./src/components/private-badge/index.js:433767/191

--- src/components/private-badge/index.js
+++ src/components/private-badge/index.js
@@ -9,14 +9,22 @@ import styles from './styles.styl';
 
 const privateText = 'Only members can view code';
 const publicText = 'Visible to everyone';
-
+const TYPES = {
+  project: {
+    privateText: 'Only members can view code';
+  const publicText = 'Visible to everyone';
+  },
+  collection: {
+    
+  }
+}
 export const PrivateBadge = () => (
   <span className={classnames(styles.projectBadge, styles.private)}>
     <TooltipContainer type="info" tooltip={privateText} target={<Icon icon="private" />} />
   </span>
 );
 
-export const PrivateToggle = ({ isPrivate, setPrivate }) => (
+export const PrivateToggle = ({ isPrivate, setPrivate, type }) => (
   <span className={classnames(styles.button, styles.projectBadge, isPrivate ? styles.private : styles.public)}>
     <TooltipContainer
       type="action"
