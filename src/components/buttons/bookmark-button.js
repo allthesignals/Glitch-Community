dce393f70880049187eea33dc27ebe648ad2ed59
Glitch (community-staging) <none>
2019-09-19 17:57:21 +0000

    🍅🍓 Checkpoint ./src/components/buttons/bookmark-button.js:433767/33

--- src/components/buttons/bookmark-button.js
+++ src/components/buttons/bookmark-button.js
@@ -152,7 +152,7 @@ const BookmarkButton = ({ action, initialIsBookmarked, containerDetails, project
   const onAnimationEnd = () => {
     setState({ ...state, isAnimating: false, isVisible: containerDetails ? containerDetails.isHoveringOnProjectItem : true });
   };
-
+  console.log(state.isBookmarked)
   const checkClassName = cx({
     check: true,
     checkAnimated: state.isAnimating,
