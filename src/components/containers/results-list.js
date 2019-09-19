4d796a1ea6e8e6c1cc9ddf6efc84fb45a3c7dca2
Glitch (functional-politician) <none>
2019-09-19 15:16:31 +0000

    🐙🚠 Checkpoint ./src/components/containers/results-list.js:867163/76

--- src/components/containers/results-list.js
+++ src/components/containers/results-list.js
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-import { Button } from '@fogcreek/shared-components';
+import { Button, Icon } from '@fogcreek/shared-components';
 
 import TransparentButton from 'Components/buttons/transparent-button';
 import styles from './results-list.styl';
@@ -52,7 +52,7 @@ export const ResultItem = ({ className, onClick, href, children, active, selecte
       {href && (
         <div className={styles.linkButtonWrap}>
           <Button as="a" size="small" href={href} target="blank" type="secondary">
-            View <Arrow />
+            View <Icon className={styles.arrow} icon="arrowRight" />
           </Button>
         </div>
       )}
