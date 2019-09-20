86416e1f5f26dd32ef48045579a1e3cdde5ef11d
Glitch (functional-politician) <none>
2019-09-20 17:33:51 +0000

    🍼💪 Checkpoint ./src/components/collection/collection-item-small.js:867163/58

--- src/components/collection/collection-item-small.js
+++ src/components/collection/collection-item-small.js
@@ -15,6 +15,7 @@ import styles from './collection-item.styl';
 
 const collectionColorStyles = (collection) => ({
   backgroundColor: collection.coverColor,
+  borderColor: collection.coverColor,
 });
 
 const CollectionCurator = ({ collection }) => {
