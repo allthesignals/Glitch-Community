b0044780de4bfe8fcad430d31668c91f7e43a8f4
Glitch (functional-politician) <none>
2019-09-23 19:43:30 +0000

    🏬🚖 Checkpoint ./src/components/collection/collection-avatar.js:867163/9

--- src/components/collection/collection-avatar.js
+++ src/components/collection/collection-avatar.js
@@ -46,7 +46,7 @@ const getComplementaryColor = (inputColor) => {
   return whiteColorContrast > blackColorContrast ? '#fff' : '#222';
 };
 
-const CollectionSearchAvatar = ({ collection }) => (
+export const CollectionSearchAvatar = ({ collection }) => (
   <svg viewBox="0 0 135 135" xmlns="http://www.w3.org/2000/svg">
   <g transform="translate(10.5 10.5)" fill="none" fill-rule="evenodd">  
 	  <rect id="back" stroke={collection.coverColor} stroke-width="2.5" fill="#FFFFFF" x="13.5" y="14" width="100" height="100" rx="5"/>
