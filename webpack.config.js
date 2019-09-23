5afdfe44fad40651260b4abac3b2638b28e4f51b
Glitch (fallacious-slice) <none>
2019-09-23 17:27:06 +0000

    🎎🌆 Checkpoint ./webpack.config.js:97325/35

--- webpack.config.js
+++ webpack.config.js
@@ -118,7 +118,7 @@ module.exports = smp.wrap({
           {
             test: /\.js$/,
             loader: 'babel-loader',
-            include: mode === 'development' ? [SRC, SHARED] : [SRC, SHARED],
+            include: mode === 'development' ? [SRC, SHARED] : [SRC, SHARED, /[\\/]shared-components[\\/]/],
             options: {
               compact: mode === 'development' ? true : false,
               configFile: path.resolve(__dirname, './.babelrc.client.json'),
