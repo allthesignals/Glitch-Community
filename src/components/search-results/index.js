9db27d46e88f8aa4ea07c9cee16e5bb6f2445793
Glitch (functional-politician) <none>
2019-09-23 21:37:42 +0000

    🖐🌑 Checkpoint ./src/components/search-results/index.js:867163/11

--- src/components/search-results/index.js
+++ src/components/search-results/index.js
@@ -140,7 +140,7 @@ function SearchResults({ query, searchResults, activeFilter, setActiveFilter }) 
         renderedGroups.map(({ id, label, results, canShowMoreResults }) => (
           <article key={id} className={styles.groupContainer}>
             <Heading tagName="h2">{label}</Heading>
-            <Grid items={results}>{(result) => <ResultComponent result={result} projectsWithUserData={projectsWithUserData} />}</Grid>
+            <Grid items={results} gap="25px">{(result) => <ResultComponent result={result} projectsWithUserData={projectsWithUserData} />}</Grid>
             {canShowMoreResults && <ShowAllButton label={label} onClick={() => setActiveFilter(id)} />}
           </article>
         ))}
