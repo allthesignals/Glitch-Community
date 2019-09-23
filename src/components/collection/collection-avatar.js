b0ca3c85b0f9e3ad031092f2c113fd577be751f9
Glitch (functional-politician) <none>
2019-09-23 19:00:34 +0000

    🎤🎍 Checkpoint ./src/components/collection/collection-avatar.js:867163/117

--- src/components/collection/collection-avatar.js
+++ src/components/collection/collection-avatar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import { getContrastWithLightText, getContrastWithDarkText } from ''
 
 import Image from 'Components/images/image';
 import { Waves, Squares, Triangles } from 'Components/collection/collection-patterns';
