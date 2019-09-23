e12e12bad85575f0d93426f38aeb82d66faaa945
Glitch (community-staging) <none>
2019-09-23 19:59:03 +0000

    lint more things

--- src/components/text/text.js
+++ src/components/text/text.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import styles from './text.styl';
-console.log(styles);
 
 const cx = classNames.bind(styles);
 
