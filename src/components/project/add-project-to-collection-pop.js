1532ee63690eac83c7c5df5ae5fd6f226054130a
Glitch (community-staging) <none>
2019-09-23 19:17:05 +0000

    ❤️🌵 Checkpoint ./src/components/project/add-project-to-collection-pop.js:433767/36

--- src/components/project/add-project-to-collection-pop.js
+++ src/components/project/add-project-to-collection-pop.js
@@ -104,7 +104,7 @@ function useCollectionSearch(query, project, collectionType) {
   const myStuffEnabled = useDevToggle('My Stuff');
 
   const searchResultsWithMyStuff = useMemo(() => {
-    const shouldPutMyStuffAtFrontOfList = myStuffEnabled && searchResults.collection && collectionType === 'user' && query.length === 0;
+    const shouldPutMyStuffAtFrontOfList = myStuffEnabled && searchResults.collection && collectionType === 'user' && query.length === 0 && searchResults.status === "ready";
     if (shouldPutMyStuffAtFrontOfList) {
       return getCollectionsWithMyStuff({ collections: searchResults.collection });
     }
