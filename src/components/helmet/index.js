798f1ee870fdf535b2e6a75d5d43c01b69f5474a
Glitch (superb-guide) <none>
2019-09-24 15:08:30 +0000

    📰🏨 Checkpoint ./src/components/helmet/index.js:97325/70

--- src/components/helmet/index.js
+++ src/components/helmet/index.js
@@ -3,11 +3,12 @@ import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet-async';
 
 const GlitchHelmet = ({ title }) => (
-
+  <Helmet>
+</Helmet>
 );
 
 GlitchHelmet.propTypes = {
-    
+  title: 'Glitch',
 };
 
 export default GlitchHelmet;
