eb5cb6cc022d88e6042781b9674ec973a7dcd554
Glitch (superb-guide) <none>
2019-09-24 17:58:34 +0000

    👯👸 Checkpoint ./src/components/helmet/index.js:97325/63

--- src/components/helmet/index.js
+++ src/components/helmet/index.js
@@ -6,19 +6,19 @@ const GlitchHelmet = ({ title, description }) => (
   <Helmet>
     <title>{title}</title>
     <meta name="description" content={description} />
-    <!-- facebook open graph tags -->
-    <meta property="og:type" content="website">
-    <meta property="og:url" content="<%= canonicalUrl %>">
-    <meta property="og:title" content="<%= socialTitle || title %>">
-    <meta property="og:description" content="<%= description %>">
-    <meta property="og:image" content="<%= image %>">
-    <!-- twitter card tags (stacks with og: tags) -->
-    <meta name="twitter:card" content="summary">
-    <meta name="twitter:site" content="@glitch">
-    <meta name="twitter:title" content="<%= socialTitle || title %>">
-    <meta name="twitter:description" content="<%= description %>">
-    <meta name="twitter:image" content="<%= image %>">
-    <meta name="twitter:url" content="<%= canonicalUrl %>">
+    {/* facebook open graph tags -->
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="<%= canonicalUrl %>" />
+    <meta property="og:title" content="<%= socialTitle || title %>" />
+    <meta property="og:description" content="<%= description %>" />
+    <meta property="og:image" content="<%= image %>" />
+    {/* twitter card tags (stacks with og: tags) */}
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@glitch" />
+    <meta name="twitter:title" content="<%= socialTitle || title %>" />
+    <meta name="twitter:description" content="<%= description %>" />
+    <meta name="twitter:image" content="<%= image %>" />
+    <meta name="twitter:url" content="<%= canonicalUrl %>" />
   </Helmet>
 );
 
