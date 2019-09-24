e519281ecfb507355ecafd484486414454451a64
Glitch (superb-guide) <none>
2019-09-24 17:05:30 +0000

    👍🦀 Checkpoint ./src/utils/markdown.js:97325/323

--- src/utils/markdown.js
+++ src/utils/markdown.js
@@ -3,22 +3,22 @@ import markdownEmoji from 'markdown-it-emoji';
 import markdownHeadings from 'markdown-it-github-headings';
 import markdownSanitizer from 'markdown-it-sanitizer';
 
-export const markdown = (content, { allowImages, linkifyHeadings }) => {
-    const mdIt = markdownIt({
-      html: true,
-      breaks: true,
-      linkify: true,
-      typographer: true,
-    }).disable('smartquotes');
-  
-    if (!allowImages) {
-      mdIt.disable('image');
-    }
-    if (linkifyHeadings) {
-      const headingOpts = { enableHeadingLinkIcons: false, prefixHeadingIds: false };
-      return mdIt.use(markdownHeadings, headingOpts).use(markdownEmoji).use(markdownSanitizer);
-    }
-    return mdIt.use(markdownEmoji).use(markdownSanitizer).render(content);
+export const renderMarkdown = (content, { allowImages, linkifyHeadings }) => {
+  const mdIt = markdownIt({
+    html: true,
+    breaks: true,
+    linkify: true,
+    typographer: true,
+  }).disable('smartquotes');
+
+  if (!allowImages) {
+    mdIt.disable('image');
+  }
+  if (linkifyHeadings) {
+    const headingOpts = { enableHeadingLinkIcons: false, prefixHeadingIds: false };
+    return mdIt.use(markdownHeadings, headingOpts).use(markdownEmoji).use(markdownSanitizer);
+  }
+  return mdIt.use(markdownEmoji).use(markdownSanitizer).render(content);
 };
   
 export const stripHtml = (html) => {
