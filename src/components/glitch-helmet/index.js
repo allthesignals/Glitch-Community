adad8fa6329a8c4a3a1c91e8712b6e9c649958c1
Glitch (superb-guide) <none>
2019-09-24 18:03:32 +0000

    🌛🍚 Checkpoint ./src/components/glitch-helmet/index.js:97325/81

--- src/components/glitch-helmet/index.js
+++ src/components/glitch-helmet/index.js
@@ -30,7 +30,7 @@ GlitchHelmet.propTypes = {
 };
 
 GlitchHelmet.defaultProps = {
-  image: '',
+  image: 'https://cdn.gomix.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fsocial-card%402x.png',
   socialTitle: null,
 };
 
