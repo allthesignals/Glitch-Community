e9b17a4f7b16c54522787a079e6bb61da2821f48
Glitch (community-staging) <none>
2019-09-24 20:33:08 +0000

    🐥🍅 Checkpoint ./src/components/collection/collection-options-pop.js:9/74

--- src/components/collection/collection-options-pop.js
+++ src/components/collection/collection-options-pop.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Popover, UnstyledButton } from '@fogcreek/shared-components';
+
 import { PopoverMenu } from 'Components/popover';
 import DeleteCollection from 'Components/collection/delete-collection-pop';
 
