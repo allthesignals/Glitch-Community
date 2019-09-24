58acdfc59fc7a6ff146a5a42e80e188e6f3f91a6
Glitch (superb-guide) <none>
2019-09-24 17:59:33 +0000

    ⛵️🌉 Checkpoint ./src/components/helmet/index.js:97325/99

--- src/components/helmet/index.js
+++ src/components/helmet/index.js
@@ -2,21 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet-async';
 
-const GlitchHelmet = ({ title, description }) => (
+const GlitchHelmet = ({ title, description, socialTitle }) => (
   <Helmet>
     <title>{title}</title>
     <meta name="description" content={description} />
-    {/* facebook open graph tags -->
+    {/* facebook open graph tags */}
     <meta property="og:type" content="website" />
     <meta property="og:url" content="<%= canonicalUrl %>" />
-    <meta property="og:title" content="<%= socialTitle || title %>" />
-    <meta property="og:description" content="<%= description %>" />
+    <meta property="og:title" content={socialTitle || title} />
+    <meta property="og:description" content={description} />
     <meta property="og:image" content="<%= image %>" />
     {/* twitter card tags (stacks with og: tags) */}
     <meta name="twitter:card" content="summary" />
     <meta name="twitter:site" content="@glitch" />
-    <meta name="twitter:title" content="<%= socialTitle || title %>" />
-    <meta name="twitter:description" content="<%= description %>" />
+    <meta name="twitter:title" content={socialTitle || title} />
+    <meta name="twitter:description" content={description} />
     <meta name="twitter:image" content="<%= image %>" />
     <meta name="twitter:url" content="<%= canonicalUrl %>" />
   </Helmet>
