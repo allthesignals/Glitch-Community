403846a4264436f2128f4b161ac3a9a36f527793
Glitch (superb-guide) <none>
2019-09-25 14:22:25 +0000

    🎃🐾 Checkpoint ./src/utils/markdown.js:97325/136 ./build/node/utils/markdown.js:97325/247

--- src/utils/markdown.js
+++ src/utils/markdown.js
@@ -25,3 +25,7 @@ export const stripHtml = (html) => {
   const regex = /<\/?([a-z][a-z0-9]*)\b[^>]*>?/gi;
   return html ? html.replace(regex, '').trim() : '';
 };
+
+export const renderText = (content) => {
+  return stripHtml(renderMarkdown(content));
+};
