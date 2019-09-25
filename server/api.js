2e19b84b3723585f69c926d3a85a8bf65f467a88
Glitch (community-staging) <none>
2019-09-25 17:15:05 +0000

    👖⛴ Checkpoint ./server/api.js:301412/1487

--- server/api.js
+++ server/api.js
@@ -74,6 +74,17 @@ async function getCultureZinePosts() {
   return response.data.posts;
 }
 
+async function getPupdatesFromPrismic() {
+  console.log('Fetching pupdates');
+  const client = 'client_id=ghost-frontend&client_secret=c9a97f14ced8';
+  
+  const params = 'filter=featured:true&limit=4&fields=id,title,url,feature_image,primary_tag&include=tags';
+  const url = `https://culture-zine.glitch.me/culture/ghost/api/v0.1/posts/?${client}&${params}`;
+  const response = await axios.get(url, { timeout: 10000 });
+  return response.data.posts;
+}
+}
+
 const [getFromCache] = createCache(dayjs.convert(1, 'hour', 'ms'), 'load');
 const [getFromZineCache] = createCache(dayjs.convert(15, 'minutes', 'ms'), 'load');
 
