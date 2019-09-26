925e5c7a3edc5454601b1dd2f7e10a19edeba5d4
Glitch (community-staging) <none>
2019-09-26 21:30:36 +0000

    format search results

--- src/models/collection.js
+++ src/models/collection.js
@@ -17,7 +17,7 @@ const nullMyStuffCollection = {
   coverColor: pickRandomColor(),
   projects: [],
   id: 'nullMyStuff',
-  private: true,
+  isPrivate: true,
 };
 
 // puts my stuff at the front of the array, if my stuff doesn't exist we add it.
