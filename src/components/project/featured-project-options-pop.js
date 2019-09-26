f1add12dba7ffe36589a08555334b69fab0f8be5
Glitch (cookie-ricotta) <none>
2019-09-26 15:39:15 +0000

    🍪🐢 Checkpoint ./src/components/project/featured-project-options-pop.js:9/73

--- src/components/project/featured-project-options-pop.js
+++ src/components/project/featured-project-options-pop.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Actions, Button, Popover } from '@fogcreek/shared-components';
+
 import { PopoverMenu, PopoverDialog, PopoverActions, PopoverMenuButton } from 'Components/popover';
 
 export default function FeaturedProjectOptionsPop({ unfeatureProject, createNote, hasNote }) {
