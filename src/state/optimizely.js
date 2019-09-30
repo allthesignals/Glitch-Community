d3da956e0ecad7ddfdb4ae42ec380d952feeffb9
Glitch (nova-chance) <none>
2019-09-30 17:50:04 +0000

    🐟👨 Checkpoint ./src/state/rollout-toggles copy.js:97325/763 ./src/state/optimizely.js:97325/54

---
+++ src/state/optimizely.js
@@ -0,0 +1,26 @@
+import React, { createContext } from 'react';
+import { setLogLevel, setLogger, logging, createInstance } from '@optimizely/optimizely-sdk';
+
+setLogLevel('warning');
+setLogger(logging.createLogger());
+
+const optimizelyClientInstance = createInstance({
+  sdkKey: process.env.OPTIMIZELY_KEY || 'SUV4dn9FiCp1osqWQUZr82',
+  datafileOptions: {
+    autoUpdate: true,
+    updateInterval: 60 * 1000, // check once per minute
+  },
+});
+
+// Usage:
+//
+// import isFeatureEnabled from 'State/rollout-toggles`
+//
+// const NewFeatureIfEnabled = () => {
+//   const showNewFeature = isFeatureEnabled('New Feature');
+//   return showNewFeature ? <NewFeature /> : null;
+// };
+
+const isFeatureEnabled = (whichToggle, entityId) => optimizelyClientInstance.isFeatureEnabled(whichToggle, entityId);
+
+export default isFeatureEnabled;
