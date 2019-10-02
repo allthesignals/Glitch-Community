3e869dc41963d4fd7a8f66b1214ea846ddfc899c
Glitch (cookie-ricotta) <none>
2019-10-02 20:22:20 +0000

    💠🚖 Checkpoint ./src/components/team-users/add-team-user.js:9/30

--- src/components/team-users/add-team-user.js
+++ src/components/team-users/add-team-user.js
@@ -176,9 +176,9 @@ const AddTeamUser = ({ members, whitelistedDomain, inviteEmail, inviteUser, setW
         <AddTeamUserPop
           members={members}
           whitelistedDomain={whitelistedDomain}
-          setWhitelistedDomain={() => { onClose(); setWhitelistedDomain(); }}
-          inviteUser={() => { onClose(); inviteUser(); }}
-          inviteEmail={() => { onClose(); inviteEmail(); }}
+          setWhitelistedDomain={(domain) => { onClose(); setWhitelistedDomain(domain); }}
+          inviteUser={(user) => { onClose(); inviteUser(user); }}
+          inviteEmail={(email) => { onClose(); inviteEmail(email); }}
         />
       )}
     </Popover>
