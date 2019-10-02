faaa85375d31a49e4741e8c9613b4777adf298f6
Glitch (community-staging) <none>
2019-10-02 17:38:09 +0000

    🐄🌞 Checkpoint ./src/components/user-dashboard/index.js:301412/35113 ./src/components/user-dashboard/styles.styl:301412/600

--- src/components/user-dashboard/index.js
+++ src/components/user-dashboard/index.js
@@ -118,11 +118,14 @@ const Ideas = ({ count }) => {
         <Heading className={styles.ideasHeading} tagName="h3">
           <Image alt="Ideas" src="https://cdn.glitch.com/179ed565-619c-4f66-b3a3-35011d202379%2Fideas.svg" />
         </Heading>
-        
-        {ideasIdx < ideas.length 
-        <Button variant="secondary" size="small" onClick={() => setIdeasIdx(ideasIdx + count)}>
-          More Ideas <Icon icon="new" />
-        </Button>
+
+        {ideas && ideasIdx + count < ideas.length ? (
+          <Button variant="secondary" size="small" onClick={() => setIdeasIdx(ideasIdx + count)}>
+            More Ideas <Icon icon="new" />
+          </Button>
+        ) : (
+          'Check back later for more ideas'
+        )}
       </div>
 
       {ideas && (
