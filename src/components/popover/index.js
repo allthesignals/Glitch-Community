f770f8c7c23be314335e5560bf7527c016010913
Glitch (cookie-ricotta) <none>
2019-10-03 20:40:49 +0000

    👰🐌 Checkpoint ./src/components/popover/index.js:9/312

--- src/components/popover/index.js
+++ src/components/popover/index.js
@@ -1,14 +1,6 @@
 import PopoverContainer from './container';
 import PopoverSearch from './search';
 
-/*
-A popover is a light, hollow roll made from an egg batter similar to
-that of Yorkshire pudding, typically baked in muffin tins or dedicated
-popover pans, which have straight-walled sides rather than angled.
-
-...also it's a [Bootstrap UI pattern](https://www.w3schools.com/bootstrap/bootstrap_popover.asp)
-*/
-
 export {
   PopoverContainer,
   PopoverSearch,
