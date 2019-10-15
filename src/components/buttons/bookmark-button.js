819f805cbae826a02a410506c57acc927bfdc603
Cassey Lottman <>
2019-10-15 13:41:49 -0500

    make it a span

--- src/components/buttons/bookmark-button.js
+++ src/components/buttons/bookmark-button.js
@@ -157,7 +157,7 @@ const BookmarkButton = ({ action, initialIsBookmarked, containerDetails, project
 
   return (
     <HiddenCheckbox value={state.isBookmarked} onChange={onClick} onFocus={onFocus} onBlur={onBlur}>
-      <div
+      <span
         className={`${styles.bookmarkButton} ${state.isFocused ? styles.focused : ''} ${state.isVisible ? styles.visible : ''}`}
         aria-label={`Add ${projectName} to My Stuff`}
         role="img"
@@ -165,7 +165,7 @@ const BookmarkButton = ({ action, initialIsBookmarked, containerDetails, project
         <Halo isAnimating={state.isAnimating} onAnimationEnd={onAnimationEnd} />
         {state.isBookmarked ? <FilledBookmark /> : <EmptyBookmark />}
         <Image className={checkClassName} src={CHECKMARK} onAnimationEnd={onAnimationEnd} alt="" width="10px" height="10px" />
-      </div>
+      </span>
     </HiddenCheckbox>
   );
 };
