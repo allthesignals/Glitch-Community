9fd9fd9ced005715e92469c7e56b0e85beebb932
Glitch (superb-guide) <none>
2019-10-16 15:31:58 +0000

    🦁🧀 Checkpoint ./src/state/rollouts.js:97325/358 ./src/components/team-analytics/index.js:97325/36

--- src/state/rollouts.js
+++ src/state/rollouts.js
@@ -1,5 +1,6 @@
-import React, { createContext, useContext, useCallback, useEffect, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 import { enums } from '@optimizely/optimizely-sdk';
+import { useCurrentUser } from 'State/current-user';
 
 const Context = createContext();
 
@@ -12,19 +13,23 @@ const useOptimizely = () => useContext(Context);
 const useOptimizelyValue = (getValue, dependencies) => {
   const optimizely = useOptimizely();
   const [value, setValue] = useState(() => getValue(optimizely));
-  const getValueCallback = useCallback(getValue, dependencies);
   useEffect(() => {
     const event = enums.NOTIFICATION_TYPES.OPTIMIZELY_CONFIG_UPDATE;
     const id = optimizely.notificationCenter.addNotificationListener(event, () => {
-      setValue(getValueCallback(optimizely));
+      setValue(getValue(optimizely));
     });
-    setValue(getValueCallback(optimizely));
+    setValue(getValue(optimizely));
     return () => optimizely.notificationCenter.removeNotificationListener(id);
-  }, [optimizely, getValueCallback]);
+  }, [optimizely, ...dependencies]);
   return value;
 };
 
-export const useFeatureEnabled = (whichToggle, entityId) => useOptimizelyValue(
+export const useFeatureEnabledForEntity = (whichToggle, entityId) => useOptimizelyValue(
   (optimizely) => optimizely.isFeatureEnabled(whichToggle, entityId),
   [whichToggle, entityId],
 );
+
+export const useFeatureEnabled = (whichToggle) => {
+  const { currentUser } = useCurrentUser();
+  return useFeatureEnabledForEntity(whichToggle, currentUser.id);
+};
