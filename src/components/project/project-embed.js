f8f004976bb488474cbdc84dec24c3bde298e438
Glitch (community-staging) <none>
2019-10-24 18:10:29 +0000

    ⏰🚆 Checkpoint ./src/components/project/project-embed.js:867163/8920 ./src/components/project/project-embed.styl:867163/1997

--- src/components/project/project-embed.js
+++ src/components/project/project-embed.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+<<<<<<< HEAD
 import classNames from 'classnames/bind';
+import { CDN_URL } from 'Utils/constants';
 
 import { Button } from '@fogcreek/shared-components';
 import { ProjectAvatar } from 'Components/images/avatar';
@@ -8,39 +10,58 @@ import { ProjectLink } from 'Components/link';
 import ProfileList from 'Components/profile-list';
 import { useProjectMembers } from 'State/project';
 import Image from 'Components/images/image';
+=======
+>>>>>>> 7902cc7257e21918faaf2ccfe4c7b02294532d64
 
 import Embed from 'Components/project/embed';
 import ReportButton from 'Components/report-abuse-pop';
-import { EditButton, RemixButton } from 'Components/project/project-actions';
-import { useTracker } from 'State/segment-analytics';
+import { useTracker, useTrackedFunc } from 'State/segment-analytics';
+import { userIsProjectMember, userIsProjectTeamMember } from 'Models/project';
 import { useCurrentUser } from 'State/current-user';
+import { useProjectMembers } from 'State/project';
 import { useProjectOptions } from 'State/project-options';
+
+import { EditButton, RemixButton, MembershipButton } from './project-actions';
 import AddProjectToCollection from './add-project-to-collection-pop';
 
 import styles from './project-embed.styl';
 
 const cx = classNames.bind(styles);
-const fullscreenImageURL = 'https://cdn.glitch.com/0aa2fffe-82eb-4b72-a5e9-444d4b7ce805%2Ffullscreen.svg?v=1571865617764';
+const fullscreenImageURL = `${CDN_URL}/0aa2fffe-82eb-4b72-a5e9-444d4b7ce805%2Ffullscreen.svg?v=1571867146900`;
 
 const ProfileListWithData = ({ project }) => {
   const { value: members } = useProjectMembers(project.id);
-  return <ProfileList layout="row" glitchTeam={project.showAsGlitchTeam} {...members} size="medium" />;
+  const { users } = members ||  {};
+  return <ProfileList layout="row" users={users} size="small" />;
 };
 
 const ProjectEmbed = ({ project, top, addProjectToCollection, loading, hideCode }) => {
   const projectOptions = useProjectOptions(project, addProjectToCollection ? { addProjectToCollection } : {});
   const { currentUser } = useCurrentUser();
-  const isMember = currentUser.projects.some(({ id }) => id === project.id);
+
+  const { value: members } = useProjectMembers(project.id);
+  const isMember = userIsProjectMember({ members, user: currentUser });
+  const canBecomeMember = userIsProjectTeamMember({ project, user: currentUser });
+
+  const [embedKey, setEmbedKey] = useState(0); // used to refresh project embed when users leave or join projects
+
   const trackRemix = useTracker('Click Remix', {
     baseProjectId: project.id,
     baseDomain: project.domain,
   });
 
+  const refreshEmbed = () => {
+    setEmbedKey(embedKey + 1);
+  };
+
+  const trackedLeaveProject = useTrackedFunc(projectOptions.leaveProject, 'Leave Project clicked');
+  const trackedJoinProject = useTrackedFunc(projectOptions.joinTeamProject, 'Join Project clicked');
+
   return (
     <section className={styles.projectEmbed}>
       {top}
       <div className={styles.embedWrap}>
-        <Embed domain={project.domain} loading={loading} hideCode={hideCode} />
+        <Embed key={embedKey} domain={project.domain} loading={loading} hideCode={hideCode} />
         {hideCode && (
           <div className={styles.embedBottomBar}>
             <span className={styles.embedLeft}>
@@ -53,27 +74,43 @@ const ProjectEmbed = ({ project, top, addProjectToCollection, loading, hideCode 
                 <ProfileListWithData project={project} />
               </span>
             </span>
-            <Button as="a" href={`https://${project.domain}.glitch.me`}>
-              <Image src={fullscreenImageURL} className={styles.fullscreenImg} height="auto" alt="fullscreen" />
+            <Button as="a" href={`https://${project.domain}.glitch.me`} variant="secondary" target="_blank">
+              <Image src={fullscreenImageURL} className={styles.fullscreenImg} height="auto" alt="fullscreen"/>
             </Button>
           </div>
         )}
       </div>
       <div className={styles.buttonContainer}>
-        <div className={styles.left}>
-          {isMember ? (
-            <EditButton name={project.id} isMember={isMember} size="small" />
-          ) : (
-            <ReportButton reportedType="project" reportedModel={project} />
+        <div>
+          <div className={styles.buttonWrap}>
+            {isMember ? (
+              <EditButton name={project.id} isMember={isMember} size="small" />
+            ) : (
+              <ReportButton reportedType="project" reportedModel={project} />
+            )}
+          </div>
+          {(projectOptions.leaveProject || projectOptions.joinTeamProject) && (
+            <div className={styles.buttonWrap}>
+              <MembershipButton
+                project={project}
+                isMember={isMember}
+                isTeamProject={canBecomeMember}
+                joinProject={trackedJoinProject}
+                leaveProject={trackedLeaveProject}
+                refreshEmbed={refreshEmbed}
+              />
+            </div>
           )}
         </div>
-        <div className={cx({ right: true, buttonWrap: true })}>
+        <div>
           {projectOptions.addProjectToCollection && (
-            <div className={styles.addToCollectionWrap}>
+            <div className={styles.buttonWrap}>
               <AddProjectToCollection project={project} addProjectToCollection={projectOptions.addProjectToCollection} fromProject />
             </div>
           )}
-          <RemixButton name={project.domain} isMember={isMember} onClick={trackRemix} />
+          <div className={styles.buttonWrap}>
+            <RemixButton name={project.domain} isMember={isMember} onClick={trackRemix} />
+          </div>
         </div>
       </div>
     </section>
