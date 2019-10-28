c8f737ab54b75d71655fc0b2a485e69c0be59fa5
Glitch (charmed-albertonykus) <none>
2019-10-28 20:13:57 +0000

    🏋🍫 Checkpoint ./src/presenters/pages/router.js:9/88

--- src/presenters/pages/router.js
+++ src/presenters/pages/router.js
@@ -185,10 +185,9 @@ const Router = () => {
           path="/settings"
           exact
           render={({ location }) =>
-            <>
-              { settingsPageEnabled ? <SettingsPage key={location.key} /> : <NotFoundPage key={location.key} />}
-            </>
-          } />
+            <>{ settingsPageEnabled ? <SettingsPage key={location.key} /> : <NotFoundPage key={location.key} />}</>
+          }
+        />
 
         <Route path="/vscode-auth" exact render={({ location }) => <VSCodeAuth key={location.key} scheme={parse(location.search, 'scheme')} />} />
 
