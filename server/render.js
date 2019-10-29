ca0c09b6cc8e16fcc408c3f8174add027b0f6898
Glitch (nova-chance) <none>
2019-10-29 20:35:22 +0000

    📷📖 Checkpoint ./server/render.js:97325/79

--- server/render.js
+++ server/render.js
@@ -82,7 +82,7 @@ const render = async (url, context) => {
   const html = ReactDOMServer.renderToString(sheet.collectStyles(page));
   const styleTags = sheet.getStyleTags();
   sheet.seal();
-  const OPTIMIZELY_DATA = await getOptimizelyData();
+  const OPTIMIZELY_DATA = await getOptimizelyData(); // grab the latest optimizely again because we didn't use the one from context
   return { ...context, OPTIMIZELY_DATA, html, helmet: helmetContext.helmet, styleTags };
 };
 
