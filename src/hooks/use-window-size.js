58860ff303dba060105e8d5e5e1ffa8be2731796
Cassey Lottman <clottman@users.noreply.github.com>
2019-10-30 16:41:10 -0500

    Palm hat (#1038)
    * fix remove user from team
    
    * remove unused parameter
    
    * 🍇🌧 Checkpoint
    ./src/hooks/use-window-size.js:708175/45

--- src/hooks/use-window-size.js
+++ src/hooks/use-window-size.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 export const mediumSmallViewport = 592; // 592px is the cutoff for hiding some of the button text for mobile
 
 export const useWindowSize = () => {
-  const [windowSize, setWindowSize] = useState([window.innerWidth, window.innerHeight]);
+  const [windowSize, setWindowSize] = useState([0, 0]);
   useEffect(() => {
     const handleResize = () => setWindowSize([window.innerWidth, window.innerHeight]);
     window.addEventListener('resize', handleResize);
