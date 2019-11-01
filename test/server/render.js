96ee25a11602053a80ce18155ffad9a2aa2c3c3b
Glitch (nova-chance) <none>
2019-11-01 19:47:17 +0000

    🔮✋ Checkpoint ./test/server/render.js:97325/1522

--- test/server/render.js
+++ test/server/render.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import chai from 'chai';
+import { renderToString } from 'react-dom/server';
 import HOME_CONTENT from 'Curated/home.json';
 import PUPDATES_CONTENT from 'Curated/pupdates.json';
+import { tests } from 'Shared/ab-tests';
 import { Page, resetState } from '../../src/server';
 import { makeTestCollection, makeTestProject, makeTestTeam, makeTestUser } from '../helpers/models';
 
-chai.should();
-
 const makeDefaultProps = () => ({
   helmetContext: {},
-  optimizely: { isFeatureEnabled: () => false },
+  optimizely: { isFeatureEnabled: () => false }, // gross :(
   origin: 'https://glitch.com',
-  AB_TESTS: {},
+  AB_TESTS: Object.keys(tests).reduce((assignments, name) => ({ ...assignments, [name]: Object.keys(tests[name])[0] }), {}),
   API_CACHE: {},
   EXTERNAL_ROUTES: [],
   HOME_CONTENT,
@@ -21,7 +20,16 @@ const makeDefaultProps = () => ({
 });
 
 describe('Server Side Rendering', function() {
-  it('should render the create page', function() {
-    const component = <Page align={['left']} type={'userCollection'} isPrivate={true} setPrivate={() => {}} />;
+  it('create page', function() {
+    renderToString(<Page {...makeDefaultProps()} route="/create" />);
+  });
+  it('signed out home page', function() {
+    renderToString(<Page {...makeDefaultProps()} route="/" SSR_SIGNED_IN={false} />);
+  });
+  it('signed in home page', function() {
+    renderToString(<Page {...makeDefaultProps()} route="/" SSR_SIGNED_IN={true} />);
+  });
+  it('project page', function() {
+    const project = makeTestProject({ users: [makeTestUser()] });
   });
 });
