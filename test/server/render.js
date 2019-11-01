080c5abebdfe78d5c4a154e2a6d0c0b136bbb737
Glitch (nova-chance) <none>
2019-11-01 21:34:33 +0000

    🐠🎲 Checkpoint ./test/server/render.js:97325/1917

--- test/server/render.js
+++ test/server/render.js
@@ -6,57 +6,69 @@ import { tests } from 'Shared/ab-tests';
 import { Page, resetState } from '../../src/server';
 import { makeTestCollection, makeTestProject, makeTestTeam, makeTestUser } from '../helpers/models';
 
-const makeDefaultProps = () => ({
-  helmetContext: {},
-  optimizely: { isFeatureEnabled: () => false }, // gross :(
-  origin: 'https://glitch.com',
-  AB_TESTS: Object.keys(tests).reduce((assignments, name) => ({ ...assignments, [name]: Object.keys(tests[name])[0] }), {}),
-  API_CACHE: {},
-  EXTERNAL_ROUTES: [],
-  HOME_CONTENT,
-  PUPDATES_CONTENT,
-  SSR_SIGNED_IN: false,
-  ZINE_POSTS: [],
-});
+function renderPage(route, props) {
+  resetState();
+  const optimizely = { isFeatureEnabled: () => false }; //gross
+  const abTests = Object.keys(tests).reduce((assignments, name) => ({
+    ...assignments,
+    [name]: Object.keys(tests[name])[0],
+  }), {});
+  const page = <Page
+    helmetContext={{}}
+    optimizely={optimizely}
+    origin="https://glitch.com"
+    route={route}
+    AB_TESTS={abTests}
+    API_CACHE={{}}
+    EXTERNAL_ROUTES={[]}
+    HOME_CONTENT={HOME_CONTENT}
+    PUPDATES_CONTENT={PUPDATES_CONTENT}
+    SSR_SIGNED_IN={false}
+    ZINE_POSTS={[]}
+    {...props}
+  />
+  renderToString(page);
+  resetState();
+}
 
 describe('Server Side Rendering', function() {
   it('signed out home page', function() {
-    renderToString(<Page {...makeDefaultProps()} route="/" SSR_SIGNED_IN={false} />);
+    renderPage('/', { SSR_SIGNED_IN: false });
   });
   it('signed in home page', function() {
-    renderToString(<Page {...makeDefaultProps()} route="/" SSR_SIGNED_IN={true} />);
+    renderPage('/', { SSR_SIGNED_IN: true });
   });
   it('create page', function() {
-    renderToString(<Page {...makeDefaultProps()} route="/create" />);
+    renderPage('/create');
   });
   it('collection page', function() {
     const projects = [makeTestProject()];
     const user = makeTestUser();
     const collection = makeTestCollection({ projects, user });
-    const apiCache = { 'collection:test-user-1/test-collection': collection };
-    renderToString(<Page {...makeDefaultProps()} route="/@test-user-1/test-collection" API_CACHE={apiCache} />);
+    const API_CACHE = { 'collection:test-user-1/test-collection': collection };
+    renderPage('/@test-user-1/test-collection', { API_CACHE });
   });
   it('project page', function() {
     const users = [makeTestUser()];
     const teams = [makeTestTeam()];
     const project = makeTestProject({ domain: 'test-project', teams, users });
-    const apiCache = { 'project:test-project': project };
-    renderToString(<Page {...makeDefaultProps()} route="/~test-project" API_CACHE={apiCache} />);
+    const API_CACHE = { 'project:test-project': project };
+    renderPage('/~test-project', { API_CACHE });
   });
   it('team page', function() {
     const collections = [makeTestCollection()];
     const projects = [makeTestProject()];
     const users = [makeTestUser()];
     const team = makeTestTeam({ url: 'test-team', collections, projects, users })
-    const apiCache = { 'team-or-user:test-team': { team } };
-    renderToString(<Page {...makeDefaultProps()} route="/@test-team" API_CACHE={apiCache} />);
+    const API_CACHE = { 'team-or-user:test-team': { team } };
+    renderPage('/@test-team', { API_CACHE });
   });
   it('user page', function() {
     const collections = [makeTestCollection()];
     const projects = [makeTestProject()];
     const teams = [makeTestTeam()];
     const user = makeTestUser({ login: 'glitch-user', collections, projects, teams })
-    const apiCache = { 'team-or-user:glitch-user': { user } };
-    renderToString(<Page {...makeDefaultProps()} route="/@glitch-user" API_CACHE={apiCache} />);
+    const API_CACHE = { 'team-or-user:glitch-user': { user } };
+    renderPage('/@glitch-user', { API_CACHE });
   });
 });
