999a630c23043ce225798fee37aaab11740c2ca7
Tiffany Tseng <scientiffic@gmail.com>
2019-11-06 12:04:29 -0500

    updated /search page with input field + show random project button

--- src/presenters/pages/search.js
+++ src/presenters/pages/search.js
@@ -3,12 +3,36 @@ import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet-async';
 import { withRouter } from 'react-router-dom';
 
+import { Icon, Button } from '@fogcreek/shared-components';
+import SearchForm from 'Components/search-form';
+import { ProjectLink } from 'Components/link';
 import SearchResults from 'Components/search-results';
-import NotFound from 'Components/errors/not-found';
 import MoreIdeas from 'Components/more-ideas';
 import Layout from 'Components/layout';
+import Heading from 'Components/text/heading';
 import { useAlgoliaSearch } from 'State/search';
 import { AnalyticsContext } from 'State/segment-analytics';
+import { useCachedCollection } from 'State/api-cache';
+import styles from './search.styl';
+
+const RandomProjectButton = () => {
+  // use community picks Glitch collection
+  const owner = 'glitch';
+  const name = 'community-picks';
+  const { value: collection } = useCachedCollection(`${owner}/${name}`);
+
+  return (
+    <>
+      {collection && (
+        <ProjectLink project={collection.projects[Math.floor(Math.random() * collection.projects.length)]}>
+          <Button>
+            Surprise Me <Icon icon="diamondSmall" />
+          </Button>
+        </ProjectLink>
+      )}
+    </>
+  );
+};
 
 const SearchPage = withRouter(({ query, activeFilter, history }) => {
   const searchResults = useAlgoliaSearch(query);
@@ -23,7 +47,13 @@ const SearchPage = withRouter(({ query, activeFilter, history }) => {
         {query ? (
           <SearchResults query={query} searchResults={searchResults} activeFilter={activeFilter || 'all'} setActiveFilter={setActiveFilter} />
         ) : (
-          <NotFound name="anything" />
+          <>
+            <Heading tagName="h1">Search millions of apps</Heading>
+            <div className={styles.emptySearchWrapper}>
+              <SearchForm />
+              <RandomProjectButton />
+            </div>
+          </>
         )}
         <MoreIdeas />
       </Layout>
