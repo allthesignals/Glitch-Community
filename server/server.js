5788b1a0d1631177cd54d7838d6d3cfc0ca6935f
Glitch (profuse-tea) <none>
2019-11-08 20:54:20 +0000

    🎬🍋 Checkpoint ./server/server.js:97325/5

--- server/server.js
+++ server/server.js
@@ -74,7 +74,7 @@ app.use((req, res, next) => {
 });
 app.use(compression());
 
-app.get('/edit', function(req, res) {
+app.get('/edit(/*)?', function(req, res) {
   res.status(500);
   res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
   res.send('Sorry, no editor for remixes!');
