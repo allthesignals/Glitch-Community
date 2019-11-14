b494e5f927b3b4c2fe85614c979e1e28afeae1f1
Glitch (superb-guide) <none>
2019-11-14 20:32:32 +0000

    ⭐️🍂 Checkpoint ./server/render.js:97325/2659

--- server/render.js
+++ server/render.js
@@ -7,8 +7,18 @@ const { getOptimizelyClient, getOptimizelyData } = require('./optimizely');
 
 const [getFromCache, clearCache] = createCache(dayjs.convert(15, 'minutes', 'ms'), 'render', { html: null, helmet: null, styleTags: null });
 
+let requireClient = () => require('../build/server');
+
 const watch = (location, entry, log) => {
   let needsReload = true;
+  requireClient = () => {
+    const startTime = performance.now();
+    const required = require(entry);
+    const endTime = performance.now();
+    if (needsReload) log(Math.round(endTime - startTime));
+    needsReload = false;
+    return required;
+  };
   // clear client code from the require cache whenever it gets changed
   // it'll get loaded off the disk again when the render calls require
   require('chokidar').watch(location).on('all', () => {
@@ -19,22 +29,21 @@ const watch = (location, entry, log) => {
     });
     // clear the server rendering cache
     clearCache();
+  }).on('ready', () => {
+    try {
+      requireClient();
+    } catch (error) {
+      // try importing right away so we don't have to wait
+      // but if it fails now it's probably because there are no past builds to use
+      console.warn('Failed to load client code for ssr. This either means the initial build is not finished or there is a bug in the code');
+      console.error(error.toString());
+      captureException(error);
+    }
   });
-  return () => {
-    const startTime = performance.now();
-    const required = require(entry);
-    const endTime = performance.now();
-    if (needsReload) log(Math.round(endTime - startTime));
-    needsReload = false;
-    return required;
-  };
 };
-
-let requireClient = () => require('../build/server');
 
 if (!process.env.BUILD_TYPE || process.env.BUILD_TYPE === 'memory') {
   const SRC = path.join(__dirname, '../src');
-
   const stylus = require('stylus');
   require('@babel/register')({
     only: [(location) => location.startsWith(SRC)],
@@ -46,42 +55,15 @@ if (!process.env.BUILD_TYPE || process.env.BUILD_TYPE === 'memory') {
       }],
     ],
   });
-  watch(SRC);
-
-  let needsTranspile = true;
-  requireClient = () => {
-    const startTime = performance.now();
-    const required = require('../src/server');
-    const endTime = performance.now();
-    if (needsTranspile) console.log(`SSR transpile took ${Math.round(endTime - startTime)}ms`);
-    needsTranspile = false;
-    return required;
-  };
+  watch(SRC, path.join(SRC, './server'), (ms) => console.log(`SSR transpile took ${ms}ms`));
 } else if (process.env.BUILD_TYPE === 'watcher') {
-  let needsReload = true;
-  requireClient = () => {
-    if (needsReload) console.log('Loading SSR bundle');
-    const required = require('../build/server');
-    needsReload = false;
-    return required;
-  };
-  watch(path.join(__dirname, '../build/server*'));
+  const BUILD = path.join(__dirname, '../build');
+  watch(path.join(BUILD, './server*'), path.join(BUILD, './server'), (ms) => console.log(`SSR load took ${ms}ms`));
 }
 
 const React = require('react');
 const ReactDOMServer = require('react-dom/server');
 const { ServerStyleSheet } = require('styled-components');
-setImmediate(() => {
-  try {
-    requireClient();
-  } catch (error) {
-    // try importing right away so we don't have to wait
-    // but if it fails now it's probably because there are no past builds to use
-    console.warn('Failed to load client code for ssr. This either means the initial build is not finished or there is a bug in the code');
-    console.error(error.toString());
-    captureException(error);
-  }
-});
 
 const render = async (url, { OPTIMIZELY_ID, ...context }) => {
   const { Page, resetState } = requireClient();
