a62c3c1ca69c5cf1f7adcc3ee6d4fd25cdaa4a31
Glitch (community-staging) <none>
2019-11-14 19:56:45 +0000

    🍃🚋 Checkpoint ./ecosystem.config.js:617503/161

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,8 @@ module.exports = {
       name: "glitch-community",
       script: "./server/server.js",
       instances : "1",
-      max_memory_restart: "1G",
-      instances : "max",
+      max_memory_restart: "1G", // This will restart gracefully the service if it takes 1GB
+      instances : "max", // This will spin up 2-3 instances
       exec_mode : "cluster",
       env: {
         NODE_ENV: "development",
