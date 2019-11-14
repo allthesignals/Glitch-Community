c0c15ee35c7dee214a2fda26d43ab9d9d836ef2b
Glitch (community-staging) <none>
2019-11-14 17:55:46 +0000

    😻📚 Checkpoint ./package.json:617503/413 ./server/webpack.js:617503/48 ./ecosystem.config.js:617503/120

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,12 @@ module.exports = {
       name: "glitch-community",
       script: "./server/server.js",
       instances : "1",
-      max_memory_restart: "1G"
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      }
     }]
 }
