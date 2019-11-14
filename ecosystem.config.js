610935f98e4fe7a2f2a6b617a386f0e0697b7988
Glitch (nettle-halloumi) <none>
2019-11-14 20:09:36 +0000

    🐸😬 Checkpoint ./ecosystem.config.js:617503/23

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,6 @@ module.exports = {
     apps : [{
       name: "glitch-community",
       script: "./server/server.js",
-      instances : "1",
       max_memory_restart: "1G", // This will restart gracefully the service if it takes 1GB
       instances : "max", // This will spin up 2-3 instances
       exec_mode : "cluster",
