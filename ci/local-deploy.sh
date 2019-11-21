5f3c43a82f310d496b0aca0909308ffb1b22cd13
cori schlegel <cori@glitch.com>
2019-11-21 16:44:57 -0600

    package.json may have been a one-time dealio. also tweaks to test deploy

--- ci/local-deploy.sh
+++ ci/local-deploy.sh
@@ -6,9 +6,7 @@ cd /opt/glitch-community
 tar -xz --overwrite -f build.tar.gz
 rm build.tar.gz
 
-#   and install the deps. was running into missing files trying to be renamed
-#   package-lock.json was _probably_ just out-of-date - might be able to stop doing it here
-rm package-lock.json && npm i   #   this is a hack, and probably not a good one.
+npm i
 
 #   per GregW
 export BUILD_TYPE=static
