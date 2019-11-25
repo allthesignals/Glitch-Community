c1e385a5d2869402674b1f0c577faa814ab26b52
Glitch (profuse-tea) <none>
2019-11-25 22:12:43 +0000

    🏁⛄️ Checkpoint ./server/curated.js:97325/1307

--- server/curated.js
+++ server/curated.js
@@ -1,23 +1,18 @@
-const fs = require('fs');
-const util = require('util');
-const path = require('path');
+const axios = require('axios');
 
-const readFile = util.promisify(fs.readFile);
-const writeFile = util.promisify(fs.writeFile);
+const api = axios.create({
+  baseURL: 'https://cms.glitch.me',
+  timeout: 15000, // nice long timeout
+});
 
-let pageCache = {};
-
-async function readCuratedContent(page) {
-  if (!pageCache[page]) {
-    const json = await readFile(path.join(__dirname, `../src/curated/${page}.json`));
-    pageCache[page] = JSON.parse(json);
-  }
-  return pageCache[page];
+async function getCultureZinePosts() {
+  console.log('Fetching culture zine posts');
+  const client = 'client_id=ghost-frontend&client_secret=c9a97f14ced8';
+  const params = 'filter=featured:true&limit=4&fields=id,title,url,feature_image,primary_tag&include=tags';
+  const url = `https://culture-zine.glitch.me/culture/ghost/api/v0.1/posts/?${client}&${params}`;
+  const response = await axios.get(url, { timeout: 10000 });
+  return response.data.posts;
 }
 
-async function writeCuratedContent({ page, data }) {
-  pageCache[page] = data;
-  await writeFile(path.join(__dirname, `../src/curated/${page}.json`), JSON.stringify(data), { encoding: 'utf8' });
-}
-
-module.exports = { readCuratedContent, writeCuratedContent };
+module.exports = {
+};
