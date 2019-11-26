6a50d31c9643f016221f596767502b54430ce3b8
cori schlegel <cori@glitch.com>
2019-11-26 12:11:56 -0600

    minor edits playing with bach invocation - some comments being teated as commands

--- ci/local-deploy.sh
+++ ci/local-deploy.sh
@@ -2,13 +2,13 @@
 set -euo pipefail
 
 #   stop serving the project first
-#   this _should_ make the host fail health checks until it's restarted
+#   this _should_ make the host fail health checks until restarted
 npm run stop && wait
 
 cd /opt/glitch-community
 rm -rf node_modules #   
 
-#   we're currently assuming we have a build file
+#   currently assuming we have a build file
 tar -xz --overwrite -f build.tar.gz
 rm build.tar.gz
 
