83bb94dee13f84e358db98d9d7f7573f10f3b357
Glitch (profuse-tea) <none>
2019-11-26 20:38:53 +0000

    🍡🚙 Checkpoint ./server/curated.js:97325/3504

--- server/curated.js
+++ server/curated.js
@@ -9,42 +9,14 @@ const api = axios.create({
   timeout: 15000, // nice long timeout
 });
 
-async function getCuratedFile(name) {
-  const response = await api.get(`/${name}.json`);
-  return response.data;
-}
-
-async function getCultureZinePosts() {
-  const client = 'client_id=ghost-frontend&client_secret=c9a97f14ced8';
-  const params = 'filter=featured:true&limit=4&fields=id,title,url,feature_image,primary_tag&include=tags';
-  const url = `https://culture-zine.glitch.me/culture/ghost/api/v0.1/posts/?${client}&${params}`;
-  // await new Promise((resolve) => setTimeout(resolve, 4000));
-  // throw new Error('oh no!');
-  const response = await api.get(url);
-  return response.data.posts;
-}
-
-const readFilePromise = util.promisify(fs.readFile);
-const writeFilePromise = util.promisify(fs.writeFile);
-async function readLocalCache(key) {
-  const json = await readFilePromise(`./.data/curated/${key}.json`, 'utf8');
-  return JSON.parse(json);
-}
-async function writeLocalCache(key, data) {
-  const json = JSON.stringify(data);
-  await writeFilePromise(`./.data/curated/${key}.json`, json, 'utf8');
-}
-
-function createCuratedUpdater(key, get, interval) {
-  let promise = readLocalCache(key);
-  promise.catch(() => console.warn(`No cached ${key} data`));
+function createUpdater(key, get, getInitial, interval) {
+  let promise = getInitial();
 
   // call get(), but only store the result once the data is actually ready
   // don't block requests for fresh data, and never replace good data with an error
   const lazyReload = async () => {
     try {
       const data = await get();
-      await writeLocalCache(key, data);
       promise = Promise.resolve(data);
     } catch (error) {
       console.warn(`Failed to load ${key}: ${error.toString()}`);
@@ -64,8 +36,30 @@ function createCuratedUpdater(key, get, interval) {
   return [getData, lazyReload];
 }
 
-const [getHomeData, reloadHomeData] = createCuratedUpdater('home', () => getCuratedFile('home'), dayjs.convert(1, 'hour', 'ms'));
-const [getPupdates, reloadPupdates] = createCuratedUpdater('pupdates', () => getCuratedFile('pupdates'), dayjs.convert(1, 'hour', 'ms'));
-const [getZine, reloadZine] = createCuratedUpdater('zine', () => getCultureZinePosts(), dayjs.convert(15, 'minutes', 'ms'));
+async function getCultureZinePosts() {
+  const client = 'client_id=ghost-frontend&client_secret=c9a97f14ced8';
+  const params = 'filter=featured:true&limit=4&fields=id,title,url,feature_image,primary_tag&include=tags';
+  const url = `https://culture-zine.glitch.me/culture/ghost/api/v0.1/posts/?${client}&${params}`;
+  // await new Promise((resolve) => setTimeout(resolve, 4000));
+  // throw new Error('oh no!');
+  const response = await api.get(url);
+  return response.data.posts;
+}
+const [getZine, reloadZine] = createUpdater('zine', getCultureZinePosts, () => [], dayjs.convert(15, 'minutes', 'ms'));
+
+async function getCuratedFile(name) {
+  const response = await api.get(`/${name}.json`);
+  return response.data;
+}
+const readFilePromise = util.promisify(fs.readFile);
+async function readLocalCache(key) {
+  const json = await readFilePromise(`src/curated/${key}.json`, 'utf8');
+  return JSON.parse(json);
+}
+function createCuratedUpdater(key, interval) {
+  return createUpdater(key, () => getCuratedFile(key), () => readLocalCache(key), interval);
+}
+const [getHomeData, reloadHomeData] = createCuratedUpdater('home', dayjs.convert(1, 'hour', 'ms'));
+const [getPupdates, reloadPupdates] = createCuratedUpdater('pupdates', dayjs.convert(1, 'hour', 'ms'));
 
 module.exports = { getHomeData, reloadHomeData, getPupdates, reloadPupdates, getZine, reloadZine };
