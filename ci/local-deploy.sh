70299b724621d3919c97e63618d544d1f98f7f21
cori schlegel <cori@glitch.com>
2019-11-26 11:53:01 -0600

    reorder some deploy steps a bit; exclude node_modules from the artifact since we'll rebuild it on the client and it may resolve the shrinkwrap issues I was seeing earlier

--- ci/local-deploy.sh
+++ ci/local-deploy.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-#   assuming we have a build file
-cd /opt/glitch-community
+#   stop serving the project first
+#   this _should_ make the host fail health checks until it's restarted
+npm run stop && wait
 
+cd /opt/glitch-community
+rm -rf node_modules #   
+
+#   we're currently assuming we have a build file
 tar -xz --overwrite -f build.tar.gz
-ls -la
 rm build.tar.gz
 
+#   install the deps, set the build type, run.
 npm i
 
 #   per GregW
 export BUILD_TYPE=static
 
-npm run stop && wait
 npm run start
