387d3acc8a84cdbc9a49ebf23f9a9795780d7920
Glitch (profuse-tea) <none>
2019-11-26 18:21:22 +0000

    😼🚃 Checkpoint ./server/curated.js:97325/1389

--- server/curated.js
+++ server/curated.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const dayjs = require('dayjs');
+const fs = require('fs');
 const { captureException } = require('@sentry/node');
 
 const api = axios.create({
@@ -16,15 +17,14 @@ async function getCultureZinePosts() {
   const client = 'client_id=ghost-frontend&client_secret=c9a97f14ced8';
   const params = 'filter=featured:true&limit=4&fields=id,title,url,feature_image,primary_tag&include=tags';
   const url = `https://culture-zine.glitch.me/culture/ghost/api/v0.1/posts/?${client}&${params}`;
-  await new Promise(resolve => setTimeout(resolve, 4000));
-  throw new Error('oh no!');
+  await new Promise((resolve) => setTimeout(resolve, 4000));
+  throw new Error('oh no!');/*
   const response = await api.get(url);
-  return response.data.posts;
+  return response.data.posts;*/
 }
 
-function createCuratedUpdater(label, get) {
-  let promise = get();
-  promise.then(() => console.log(`Loaded ${label}`));
+function createCuratedUpdater(key, get) {
+  let promise = ;
 
   // call get() again, but only store the promise once the data is actually ready
   // don't block requests for fresh data, and never replace good data with an error
@@ -33,7 +33,7 @@ function createCuratedUpdater(label, get) {
       const value = await get();
       promise = Promise.resolve(value);
     } catch (error) {
-      console.warn(`Failed to load ${label}: ${error.toString()}`);
+      console.warn(`Failed to load ${key}: ${error.toString()}`);
       captureException(error);
     }
   };
@@ -41,18 +41,17 @@ function createCuratedUpdater(label, get) {
   const getValue = () => promise;
 
   // reload on a regular interval, but wait until the last request finishes before starting the timer for the next request
-  const interval = dayjs.convert(5, 'minutes', 'ms');
   const cycle = async () => {
     await lazyReload();
-    setTimeout(cycle, interval);
+    setTimeout(cycle, dayjs.convert(5, 'minutes', 'ms'));
   };
-  setTimeout(cycle, interval);
+  cycle();
 
   return [getValue, lazyReload];
 }
 
-const [getHomeData, reloadHomeData] = createCuratedUpdater('home data', () => getCuratedFile('home'));
+const [getHomeData, reloadHomeData] = createCuratedUpdater('home', () => getCuratedFile('home'));
 const [getPupdates, reloadPupdates] = createCuratedUpdater('pupdates', () => getCuratedFile('pupdates'));
-const [getZine, reloadZine] = createCuratedUpdater('culture zine', () => getCultureZinePosts());
+const [getZine, reloadZine] = createCuratedUpdater('zine', () => getCultureZinePosts());
 
 module.exports = { getHomeData, reloadHomeData, getPupdates, reloadPupdates, getZine, reloadZine };
