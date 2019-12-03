6d3300a673e3452f84f060b0885138ab612a7c5d
Glitch (profuse-tea) <none>
2019-12-03 18:45:38 +0000

    🔨🌶 Checkpoint ./sh/update-cms.sh:97325/384

--- sh/update-cms.sh
+++ sh/update-cms.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Pulls the latest content from prismic and saves it in the repository
-# It does not commit the changes, that's still up to you :)
+# These copies are used when there is an error loading more recent content
+# This script does not commit the changes, that's still up to you :)
 
 wget -O src/curated/home.json 'https://cms.glitch.me/home.json'
 wget -O src/curated/pupdates.json 'https://cms.glitch.me/pupdates.json'
