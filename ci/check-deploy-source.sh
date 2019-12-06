db517cb8f71d404ec0a28feaaf5d9a7cb52eeb2b
cori schlegel <cori@glitch.com>
2019-12-06 12:32:49 -0600

    attempt checking the asset source

---
+++ ci/check-deploy-source.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -xeuo pipefail
+
+#   TODO
+#   *   parameterize the bootstrap bucket
+
+# check req params - we need a sha to use for file manipulation
+if [ -z "$1" ]; then
+  >&2 echo "Usage:"
+  >&2 echo "./$(basename $0) sha"
+  exit 1
+fi
+
+export CIRCLE_SHA=$1
+
+#   check S3 for the asset; tell caller the result
+"aws s3 ls s3://community-bootstrap-bucket20191205165831056600000001 | grep $CIRCLE_SHA"; code=$?
+
+if [[ "$code" == 0 ]]; then
+    echo "S3"
+else
+    echo "CIRCLE"
+fi
