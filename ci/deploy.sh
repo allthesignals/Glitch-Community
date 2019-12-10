ada4facac8929cf8c79d4e7791356166acbb5c99
cori schlegel <cori@glitch.com>
2019-12-10 16:11:07 -0600

    try trapping later again

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -41,16 +41,16 @@ do
 
   }
 
-  trap 'catch $name $CIRCLE_SHA' ERR
-
   echo $name
 
-  # we expect an error code here for the first host of any deploy
 #  set +e
   #check if the asset is already in S3
   ssh -o 'ProxyJump jump.staging.glitch.com' -o StrictHostKeyChecking=no "$name.staging" "bash --login -c \"cd /opt/glitch-community && ci/check-deploy-source.sh $CIRCLE_SHA\"" > /dev/null 2>&1; code=$?
   S3_LOOKUP_RESULT="$code"
 #  set -e
+
+  # we expect an error code above for the first host of any deploy
+  trap 'catch $name $CIRCLE_SHA' ERR
 
   echo "$S3_LOOKUP_RESULT"
   if [[ "$S3_LOOKUP_RESULT" ]]; then
