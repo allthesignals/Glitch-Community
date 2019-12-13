917abd69e7d10707987c721643023958843cd841
Glitch (quixotic-rocket) <none>
2019-12-13 21:08:14 +0000

    🐴✈️ Checkpoint ./server/webpack.js:97325/64

--- server/webpack.js
+++ server/webpack.js
@@ -4,7 +4,7 @@ module.exports = function webpackExpressMiddleware() {
   const compiler = webpack(webpackConfig);
 
   const webpackMiddleware = require('webpack-dev-middleware');
-  const stats = { children: false, errors: true, errorDetails: true };
+  const stats = { children: false };
   const middleware = webpackMiddleware(compiler, { stats, writeToDisk: true });
 
   let ready = false;
