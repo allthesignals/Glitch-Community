fc29f3a11e56a53c6830f56181a15051afb9fdd5
cori schlegel <cori@glitch.com>
2019-12-16 15:18:01 -0600

    fix up some env vars and recoonnect bootstrap to the reevised deploy

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -4,7 +4,7 @@ set -x
 
 #####
 # THIS SCRIPT RUNS ON THE CIRCLE CI EXECUTOR
-# It marshalls remote calls to devices inside our infrastructure where the work occurs
+# It marshalls remote calls to devices inside our infrastructure where some of the work occurs
 #####
 
 # check req params - we need a sha to use for file manipulation later
@@ -18,26 +18,26 @@ export CIRCLE_SHA=$1
 source ci/env
 set -u  # -u not set above because we expect ENVIRONMENT to be unset when ci/env is called from CircleCI
 
-./ci/publish-build-asset.sh "$ENVIRONMENT" "$CIRCLE_SHA" 
+./ci/publish-build-asset.sh "${ENVIRONMENT}" "${CIRCLE_SHA}" 
 
 # first get the list of hostnames - we could do this on any host, but we know the worker has the code
-HOSTNAMES=( $(ssh -q "worker.$ENVIRONMENT" "bash --login -c 'cd /opt/glitch && ci/hostnames-by-role community $ENVIRONMENT'") )
+HOSTNAMES=( $(ssh -q "worker.${ENVIRONMENT}" "bash --login -c 'cd /opt/glitch && ci/hostnames-by-role community ${ENVIRONMENT}'") )
 
 echo "${HOSTNAMES[@]}"
 
 for name in ${HOSTNAMES[*]}
 do
 
-  echo "$name"
+  echo "${name}"
 
   # do the "local" deploy stuff
-  ssh -o "ProxyJump $JUMP_DOMAIN" -o StrictHostKeyChecking=no "$name.$ENVIRONMENT" "bash --login -c \"cd /opt/glitch-community && ci/local-deploy.sh $ENVIRONMENT $CIRCLE_SHA\""; code=$?
+  ssh -o "ProxyJump $JUMP_DOMAIN" -o StrictHostKeyChecking=no "$name.${ENVIRONMENT}" "bash --login -c \"cd /opt/glitch-community && ci/local-deploy.sh ${ENVIRONMENT} ${CIRCLE_SHA} ${COMMUNITY_AWS_BOOTSTRAP_SECRET}\""; code=$?
 
 done
 
-if [ $code -ne 0 ]; then
+if [ ${code} -ne 0 ]; then
   echo "Deploy failed"
-  exit $code
+  exit ${code}
 fi
 
 echo "Deploy succeeded"
--- ci/local-deploy.sh
+++ ci/local-deploy.sh
@@ -9,34 +9,37 @@ set -xeuo pipefail
 #####
 
 # check req params - we need the env and a sha to use for file manipulation
-# fewer than 2 params is an error
-if [ 2 -ne "$#" ]; then
+# fewer than 3 params is an error
+if [ 3 -ne "$#" ]; then
   >&2 echo "Usage:"
-  >&2 echo "./$(basename $0) environment sha"
+  >&2 echo "./$(basename $0) environment sha bootstrap_bucket_secret"
   exit 1
 fi
 
-export ENVIRONMENT=$1
-export CIRCLE_SHA=$2
+export ENVIRONMENT="$1"
+export CIRCLE_SHA="$2"
+export COMMUNITY_AWS_BOOTSTRAP_SECRET="$3"
 
 cd /opt/glitch-community
 
-source ci/env
+source ./ci/env
+export AWS_ACCESS_KEY_ID=${AWS_BOOTSTRAP_KEY}
+export AWS_SECRET_ACCESS_KEY=${AWS_BOOTSTRAP_SECRET}
 
 # we run npm i here to ensure pm2 is available and the npm script doesn't throw
 npm i && npm run stop && wait
 
 # prevent cruft like deleted files from hanging around; currently removed folders will still persist
 # do NOT remove the asset or the ci scripts.
-find . -type f | grep -v -e "$CIRCLE_SHA.tar.gz" -e "ci" | xargs rm -rf
+find . -type f | grep -v -e "${CIRCLE_SHA}.tar.gz" -e "ci" | xargs rm -rf
 
 # go get the build file. we assume it is there; the only caller checks first. then deploy
-aws s3 cp --quiet "s3://$BOOTSTRAP_BUCKET/$CIRCLE_SHA.tar.gz" .
-tar -xz --overwrite -f "$CIRCLE_SHA.tar.gz"
-rm "$CIRCLE_SHA.tar.gz"
+aws s3 cp --quiet "s3://${BOOTSTRAP_BUCKET}/${CIRCLE_SHA}.tar.gz" .
+tar -xz --overwrite -f "${CIRCLE_SHA}.tar.gz"
+rm "${CIRCLE_SHA}.tar.gz"
 
 #   retrieve the stored .env file
-aws s3 cp --quiet s3://$BOOTSTRAP_BUCKET/.env .
+aws s3 cp --quiet s3://${BOOTSTRAP_BUCKET}/.env .
 source .env
 
 #   install the deps, run the app
