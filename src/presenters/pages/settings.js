a6214e5d8e53b420d26e70fdaf6ab64093331215
Tara Vancil <contact@taravancil.com>
2020-01-07 09:37:56 -0500

    add <SubscriptionSettings> to /settings, use React Tabs

--- src/presenters/pages/settings.js
+++ src/presenters/pages/settings.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import { Button, Icon } from '@fogcreek/shared-components';
 
 import Layout from 'Components/layout';
@@ -6,6 +7,7 @@ import GlitchHelmet from 'Components/glitch-helmet';
 import Heading from 'Components/text/heading';
 import PasswordSettings from 'Components/account-settings-overlay/password-settings';
 import TwoFactorSettings from 'Components/account-settings-overlay/two-factor-settings';
+import SubscriptionSettings from 'Components/account-settings-overlay/subscription-settings';
 import DeleteSettings from 'Components/delete-account/delete-account-modal';
 import useDevToggle from 'State/dev-toggles';
 import { useCurrentUser } from 'State/current-user';
@@ -15,6 +17,7 @@ import styles from './settings.styl';
 import { emoji } from '../../components/global.styl';
 
 const Settings = () => {
+  const [currentTab, setCurrentTab] = useState(0);
   const tagline = 'Account Settings';
   const userPasswordEnabled = useDevToggle('User Passwords');
   const tfaEnabled = useDevToggle('Two Factor Auth');
@@ -28,22 +31,60 @@ const Settings = () => {
     return <NotFoundPage />;
   }
 
+  const AccountSettingsTab = () => (
+    <>
+      {userPasswordEnabled && (
+        <div className={styles.contentSection}>
+          <PasswordSettings />
+        </div>
+      )}
+      {tfaEnabled && (
+        <div className={styles.contentSection}>
+          <TwoFactorSettings />
+        </div>
+      )}
+      {deleteEnabled && (
+        <div className={styles.contentSection}>
+          <DeleteSettings />
+        </div>
+      )}
+    </>
+  );
+
+  const SubscriptionSettingsTab = () => (
+    <div className={styles.contentSection}>
+      <SubscriptionSettings />
+    </div>
+  );
+
   return (
     <main id="main">
       <GlitchHelmet title={`Glitch - ${tagline}`} description={tagline} />
       <Layout>
-        <Heading tagName="h1">Settings <Icon className={emoji} icon="key" /></Heading>
-        <div className={styles.settingsPage}>
-          <div className={styles.settingsActions}>
-            <Button onClick={() => {}}>Account</Button>
-            {/* <Button disabled onClick>Privacy & Notifications</Button> */}
+        <Heading tagName="h1">
+          Settings <Icon className={emoji} icon="key" />
+        </Heading>
+        <Tabs selectedIndex={currentTab} onSelect={(tabIndex) => setCurrentTab(tabIndex)}>
+          <div className={styles.settingsPage}>
+            <TabList className={styles.settingsActions}>
+              <Tab>
+                <Button>Account</Button>
+              </Tab>
+              <Tab>
+                <Button>Subscription</Button>
+              </Tab>
+              {/* <Button disabled onClick>Privacy & Notifications</Button> */}
+            </TabList>
+            <div className={styles.settingsContent}>
+              <TabPanel key={0} hidden={currentTab !== 0}>
+                <AccountSettingsTab />
+              </TabPanel>
+              <TabPanel key={1} hidden={currentTab !== 1}>
+                <SubscriptionSettingsTab />
+              </TabPanel>
+            </div>
           </div>
-          <div className={styles.settingsContent}>
-            { userPasswordEnabled && <div className={styles.contentSection}><PasswordSettings /></div> }
-            { tfaEnabled && <div className={styles.contentSection}><TwoFactorSettings /></div> }
-            { deleteEnabled && <div className={styles.contentSection}><DeleteSettings /></div> }
-          </div>
-        </div>
+        </Tabs>
       </Layout>
     </main>
   );
