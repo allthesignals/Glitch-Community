49d3a5fea841d7f1f2722c1585558c72b20f5feb
Tara Vancil <contact@taravancil.com>
2020-01-09 12:58:13 -0500

    wire up /pricing with placeholder content

---
+++ src/presenters/pages/pricing.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Layout from 'Components/layout';
+import GlitchHelmet from 'Components/glitch-helmet';
+
+const PricingPage = () => {
+  return (
+    <Layout>
+      <GlitchHelmet title="/pricing title TODO" description="/pricing description TODO" canonicalUrl="/pricing" />
+      <main id="main" aria-label="Glitch Pricing Page">
+        <h1>Pricing</h1>
+      </main>
+    </Layout>
+  );
+};
+
+export default PricingPage;
