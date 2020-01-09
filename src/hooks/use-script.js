113d76c02fb3fc952da040cdd23379bacd9bda8b
Tara Vancil <contact@taravancil.com>
2020-01-09 10:48:49 -0500

    fix bug where Stripe would not be initialized on second mount
    This fixes a bug in the useStripe hook, where Stripe would only be
    initialized when the Stripe script loaded. This was a problem because
    the useScript hook only loads a given script once, so on subsequent
    mounts, the script tag would already be in the document head, and thus
    would not trigger the load event.
    
    This adds a loaded state variable to the useScript hook which is set to
    truthy if:
    
    - The script is already in the document head
    - The script is added to the head and its load event fires
    
    The useScript hook returns [loaded], so it can be consumed by container
    components.

--- src/hooks/use-script.js
+++ src/hooks/use-script.js
@@ -1,8 +1,9 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const useScript = (src) => {
   const scripts = document.getElementsByTagName('script');
   const loadedScripts = new Set();
+  const [loaded, setLoaded] = useState(false);
 
   for (const script of scripts) {
     if (script.src) {
@@ -12,13 +13,21 @@ const useScript = (src) => {
 
   useEffect(() => {
     const script = document.createElement('script');
-    if (!loadedScripts.has(src)) {
+
+    if (loadedScripts.has(src)) {
+      setLoaded(true);
+    } else {
       script.src = src;
       script.async = true;
+      script.addEventListener('load', () => {
+        setLoaded(true);
+      });
+
       document.head.appendChild(script);
       loadedScripts.add(src);
     }
   }, [src]);
+  return [loaded];
 };
 
 export default useScript;
--- src/state/stripe.js
+++ src/state/stripe.js
@@ -5,13 +5,11 @@ export default function useStripe() {
   const [stripe, setStripe] = useState(null);
   const stripeJS = 'https://js.stripe.com/v3/';
 
-  useScript(stripeJS);
+  const [loaded] = useScript(stripeJS);
   useEffect(() => {
-    const initStripe = () => {
+    if (loaded) {
       setStripe(window.Stripe('pk_test_WIJxKl0T1xT8zSb2StHu8zlo'));
-    };
-    const script = document.querySelector(`script[src="${stripeJS}"]`);
-    script.addEventListener('load', initStripe);
-  });
+    }
+  }, [loaded]);
   return stripe;
 }
