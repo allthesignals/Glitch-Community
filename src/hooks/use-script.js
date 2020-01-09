ad986787e185061822b1b891ae854feff0f8fbb2
Tara Vancil <contact@taravancil.com>
2020-01-09 08:45:27 -0500

    check if script has been written to head in useScript hook
    My commit message in 0a89f5dd57dc16271957e32fd9db37ce8bcfb46c was not
    entirely accurate. In many cases, the browser will not duplicate
    requests for duplicate script tags, but for resources with short
    max-ages (Stripe.js) will be re-requested each time they're written to
    the document.

--- src/hooks/use-script.js
+++ src/hooks/use-script.js
@@ -1,15 +1,23 @@
 import { useEffect } from 'react';
 
 const useScript = (src) => {
+  const scripts = document.getElementsByTagName('script');
+  const loadedScripts = new Set();
+
+  for (const script of scripts) {
+    if (script.src) {
+      loadedScripts.add(script.src);
+    }
+  }
+
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = src;
-    script.async = true;
-    document.head.appendChild(script);
-
-    return () => {
-      document.head.removeChild(script);
-    };
+    if (!loadedScripts.has(src)) {
+      script.src = src;
+      script.async = true;
+      document.head.appendChild(script);
+      loadedScripts.add(src);
+    }
   }, [src]);
 };
 
