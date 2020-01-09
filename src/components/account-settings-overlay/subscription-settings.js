7eaa38a4bb23b7dbb4eb3f9b2725c5a9225b69b4
Tara Vancil <contact@taravancil.com>
2020-01-09 09:39:53 -0500

    add effect that initiolizes Stripe + stripe state

--- src/components/account-settings-overlay/subscription-settings.js
+++ src/components/account-settings-overlay/subscription-settings.js
@@ -8,8 +8,17 @@ import useScript from 'Hooks/use-script';
 function SubscriptionSettings() {
   // Use temporary state variable to toggle subscription status
   const [subscribed, setSubscribed] = useState(false);
+  const [stripe, setStripe] = useState(null);
 
   useScript('https://js.stripe.com/v3/');
+
+  useEffect(() => {
+    const initStripe = () => {
+      setStripe(window.Stripe('pk_test_WIJxKl0T1xT8zSb2StHu8zlo'));
+    };
+    const script = document.querySelector('script[src="https://js.stripe.com/v3/"]');
+    script.addEventListener('load', initStripe);
+  });
 
   return (
     <>
@@ -17,14 +26,14 @@ function SubscriptionSettings() {
       {subscribed ? (
         <>
           <Text defaultMargin>Subscribed to the Extra Memory monthly plan for $14 per month.</Text>
-          <Button variant="secondary" onClick={() => setSubscribed(false)}>
+          <Button disabled={!stripe} variant="secondary" onClick={() => setSubscribed(false)}>
             Cancel Subscription
           </Button>
         </>
       ) : (
         <>
           <Text defaultMargin>Subscribe to the Extra Memory monthly plan for $14 per month.</Text>
-          <Button variant="cta" onClick={() => setSubscribed(true)}>
+          <Button disabled={!stripe} variant="cta" onClick={() => setSubscribed(true)}>
             Subscribe
           </Button>
         </>
