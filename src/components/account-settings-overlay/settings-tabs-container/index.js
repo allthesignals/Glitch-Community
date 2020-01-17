0ddf0ee803dee736bdf22b451a341ae791598b2d
Glitch (community-staging) <none>
2020-01-17 16:56:35 +0000

    🏭⛰ Checkpoint ./src/components/account-settings-overlay/settings-tabs-container/index.js:271885/29770

--- src/components/account-settings-overlay/settings-tabs-container/index.js
+++ src/components/account-settings-overlay/settings-tabs-container/index.js
@@ -11,74 +11,73 @@ import useDevToggle from 'State/dev-toggles';
 
 import styles from './styles.styl';
 
-function SettingsTabsContainer() {
+const tabs = [
+  {
+    id: 'account',
+    label: 'Account',
+    isSelectable: () => true,
+    component: () => {
+      const userPasswordEnabled = useDevToggle('User Passwords');
+      const tfaEnabled = useDevToggle('Two Factor Auth');
+      const deleteEnabled = useDevToggle('Account Deletion');
+      return (
+        <>
+          {userPasswordEnabled && (
+            <div className={styles.tabPanelSection}>
+              <PasswordSettings />
+            </div>
+          )}
+          {tfaEnabled && (
+            <div className={styles.tabPanelSection}>
+              <TwoFactorSettings />
+            </div>
+          )}
+          {deleteEnabled && (
+            <div className={styles.tabPanelSection}>
+              <DeleteSettings />
+            </div>
+          )}
+        </>
+      );
+    },
+  },
+  {
+    id: 'glitchPro',
+    label: 'Glitch PRO',
+    isSelectable: ({ showSubscriptionTab }) => showSubscriptionTab,
+    component: () => (
+      <div className={styles.tabPanelSection}>
+        <SubscriptionSettings />
+      </div>
+    ),
+  },
+];
+
+export function SettingsTabs({ page = tabs[0].id }) {
   const showSubscriptionTab = useFeatureEnabled('pufferfish');
   const userPasswordEnabled = useDevToggle('User Passwords');
   const tfaEnabled = useDevToggle('Two Factor Auth');
   const deleteEnabled = useDevToggle('Account Deletion');
+  
+  const selectableTabs = tabs.filter(tab => tab.isSelectable({ showSubscriptionTab }))
 
   return (
-    <SettingsTabs
-      userPasswordEnabled={userPasswordEnabled}
-      tfaEnabled={tfaEnabled}
-      deleteEnabled={deleteEnabled}
-      showSubscriptionTab={showSubscriptionTab}
-    />
-  );
-}
-
-const AccountSettingsTab = () => {
-  const userPasswordEnabled = useDevToggle('User Passwords');
-  const tfaEnabled = useDevToggle('Two Factor Auth');
-  const deleteEnabled = useDevToggle('Account Deletion');
-  return (
-    <>
-      {userPasswordEnabled && (
-        <div className={styles.tabPanelSection}>
-          <PasswordSettings />
-        </div>
-      )}
-      {tfaEnabled && (
-        <div className={styles.tabPanelSection}>
-          <TwoFactorSettings />
-        </div>
-      )}
-      {deleteEnabled && (
-        <div className={styles.tabPanelSection}>
-          <DeleteSettings />
-        </div>
-      )}
-    </>
-  );
-};
-
-const SubscriptionSettingsTab = () => (
-  <div className={styles.tabPanelSection}>
-    <SubscriptionSettings />
-  </div>
-);
-
-export function SettingsTabs({ userPasswordEnabled, tfaEnabled, deleteEnabled, showSubscriptionTab }) {
-  const [currentTab, setCurrentTab] = useState(0);
-
-  return (
-    <Tabs selectedIndex={currentTab} onSelect={(tabIndex) => setCurrentTab(tabIndex)}>
+    <Tabs selectedIndex={page}>
       <div className={styles.tabsContainer}>
         <TabList className={styles.tabs}>
-          <Tab key="account" data-tab="account" className={styles.tab}>
-            <Button as="span">Account</Button>
-          </Tab>
-          <Tab key="subscription" data-tab="subscription" hidden={!showSubscriptionTab} className={styles.tab}>
-            <Button as="span">Subscription</Button>
-          </Tab>
+          {selectableTabs.map((tab) => (
+            <Tab key={tab.id} data-tab={tab.id} className={styles.tab} component={Link} to={`/settings/${tab.id}`}>
+              <Button as="span">{tab.label}</Button>
+            </Tab>
+          ))}
         </TabList>
         <div className={styles.tabPanels}>
-          <TabPanel key="account" data-tabpanel="account" hidden={currentTab !== 0}>
-            <AccountSettingsTab />
-          </TabPanel>
-          <TabPanel key="subscription" data-tabpanel="subscription" hidden={currentTab !== 1}>
-            <SubscriptionSettingsTab />
-          </TabPanel>
+          {selectableTabs.map(({ id, component: Component }) => (
+            <TabPanel key={id} data-tabpanel={id} hidden={page !== id}>
+              <Component />
+            </TabPanel>
+          ))}
+         
         </div>
       </div>
     </Tabs>
@@ -92,4 +91,4 @@ SettingsTabs.defaultProps = {
   showSubscriptionTab: false,
 };
 
-export default SettingsTabsContainer;
+export default SettingsTabs;
